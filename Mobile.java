391e9d4cc025b8f45209fd9782f52419a067ac40
raguram63 <109017255+raguram63@users.noreply.github.com>
2022-07-10 13:41:23 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Mobile {
+	
+	//Method
+	public static void Makecall() {
+		
+	
+	}
+	
+	public static void sendMsg() {
+		
+		
+	}
+	
+  
+	//Main Method
+	public static void main(String[] args) {
+		
+		boolean makecall = true;
+		System.out.println("Call forwarding  :"  +makecall);
+		
+		boolean sendMsg = false;
+		System.out.println("Message sending  :"   +sendMsg);
+		
+		String mymobile="this is my Mobile";
+		System.out.println(mymobile);
+	
+	}
+	
+	
+}
